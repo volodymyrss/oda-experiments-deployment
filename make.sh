8ad88088749792fad2c5199d55e8e11d8a821e48
Volodymyr SAVCHENKO <contact@volodymyrsavchenko.com>
2020-04-14 17:41:02 +0200

    developed development

--- make.sh
+++ make.sh
@@ -1,1 +1,3 @@
 kubectl create secret generic db-user-pass  --from-file=./private/password.txt
+kubectl create secret generic odatests-tests-bot-password  --from-file=./private/testbot-password.txt
+kubectl create secret generic odatests-secret-key  --from-file=./private/secret-key.txt
