d52b211d899b00cacd3d405208525ec56a6180e5
Volodymyr SAVCHENKO <contact@volodymyrsavchenko.com>
2020-04-19 11:18:02 +0200

    more secrets, etc

--- make.sh
+++ make.sh
@@ -1,3 +1,23 @@
-kubectl create secret generic db-user-pass  --from-file=./private/password.txt
-kubectl create secret generic odatests-tests-bot-password  --from-file=./private/testbot-password.txt
-kubectl create secret generic odatests-secret-key  --from-file=./private/secret-key.txt
+
+function create-secrets(){
+    kubectl create secret generic db-user-pass  --from-file=./private/password.txt
+    kubectl create secret generic odatests-tests-bot-password  --from-file=./private/testbot-password.txt
+    kubectl create secret generic odatests-secret-key  --from-file=./private/secret-key.txt
+    kubectl create secret generic minio-key  --from-file=./private/minio-key.txt
+    kubectl create secret generic jena-password  --from-file=./private/jena-password.txt
+}
+
+function template() {
+    (cd odatests; make image-name)
+
+    < odatests-deployment-template.yaml sed 's@{{IMAGE}}@'$(cat odatests/image-name)'@' > odatests-deployment.yaml
+}
+
+function deploy() {
+    template
+
+    kubectl apply -f odatests-deployment.yaml
+    rm -f odatests-deployment.yaml
+}
+
+$@
