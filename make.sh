37e8dbd2a8ce922554477f72336c57277c4d8ad9
Volodymyr SAVCHENKO <contact@volodymyrsavchenko.com>
2020-05-16 21:19:04 +0200

    basic chart

--- make.sh
+++ make.sh
@@ -5,12 +5,13 @@ function create-secrets(){
     kubectl create secret generic odatests-secret-key  --from-file=./private/secret-key.txt
     kubectl create secret generic minio-key  --from-file=./private/minio-key.txt
     kubectl create secret generic jena-password  --from-file=./private/jena-password.txt
+    kubectl create secret generic logstash-entrypoint  --from-file=./private/logstash-entrypoint.txt
 }
 
 function template() {
     (cd odatests; make image-name)
 
-    < odatests-deployment-template.yaml sed 's@{{IMAGE}}@'$(cat odatests/image-name)'@' > odatests-deployment.yaml
+    < odatests-deployment-template.yaml sed 's@{{IMAGE}}@'$(cat image-name)'@' > odatests-deployment.yaml
 }
 
 function deploy() {
