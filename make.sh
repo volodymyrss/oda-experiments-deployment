dceb428bd5ed2dff13ec355225f91227e1d704be
Volodymyr Savchenko <contact@volodymyrsavchenko.com>
2022-01-06 18:08:03 +0100

    interpret goal

--- make.sh
+++ make.sh
@@ -3,7 +3,9 @@ function create-secrets(){
     kubectl create secret generic db-user-pass  --from-file=./private/password.txt
     kubectl create secret generic odatests-tests-bot-password  --from-file=./private/testbot-password.txt
     kubectl create secret generic odatests-secret-key  --from-file=./private/secret-key.txt
-    kubectl create secret generic minio-key  --from-file=./private/minio-key.txt
+    kubectl create secret generic minio-key-oda-experiments --from-file=./private/minio-key.txt
+    
+    kubectl delete secret jena-password || echo "no secret yet, will create"
     kubectl create secret generic jena-password  --from-file=./private/jena-password.txt
 
     kubectl delete secret logstash-entrypoint || echo "no secret yet, will create"
